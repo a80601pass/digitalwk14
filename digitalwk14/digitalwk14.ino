e0e03960479523382611ad530d8de9b0d572546f
a80601pass <39110721+a80601pass@users.noreply.github.com>
2018-05-30 11:14:35 +0800

    Add files via upload

---
+++ digitalwk14/digitalwk14.ino
@@ -0,0 +1,49 @@
+int latchPin = 8;
+int clockPin = 12;
+int dataPin = 11;
+byte colDataMatrix[8] = { B01111111,
+   B10111111,
+   B11011111,
+   B11101111,
+   B11110111,
+   B11111011,
+   B11111101,
+   B11111110
+  };
+                      
+byte rowDataMatrix[8] = { B00111100,
+            B01100010,
+    B11101001,
+    B11100001,
+    B11110001,
+    B11011001,
+    B01111110,
+    B00111100,
+  };
+// 定義顯示器顯示的時間 (延遲時間), 預設 1ms
+int delay_time = 1;
+       
+void setup() {
+pinMode(latchPin,OUTPUT);
+pinMode(clockPin,OUTPUT);
+pinMode(dataPin,OUTPUT);
+}
+void loop() {
+ for(int i = 0; i < 8 ; i++){
+ //byte colData = colDataMatrix[i];
+digitalWrite(latchPin,LOW);
+  //  pickDigit(i);
+   shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+    shiftOut(dataPin, clockPin, MSBFIRST, rowDataMatrix[i]);
+    //shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+    digitalWrite(latchPin,HIGH);
+    delay(delay_time);
+  }
+}
+void pickDigit(int x) {
+  byte rowByte = 1 << x;
+  shiftOut(dataPin, clockPin, MSBFIRST, rowByte);
+
+}
+
+
